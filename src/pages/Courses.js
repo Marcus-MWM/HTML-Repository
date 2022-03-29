8ed7e96438dfd0048e80e521ef83fb8037df0d84
mattbaisden <matthewray.baisden@calbaptist.edu>
2022-03-28 17:57:10 -0700

    courses edits / created header

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -6,9 +6,9 @@ import Header from "../components/Header"
 function Courses() {
   return (
     <>
-      <Header text ='hello testing.... '/>
+      
         <div>
-          <h1> Welcome to the Courses Page </h1>
+          <h1> Courses </h1>
         </div>
     </>
   );
