2e4a5dc58b062ad63948f7671736443535f045c3
mattbaisden <matthewray.baisden@calbaptist.edu>
2022-04-06 20:01:52 -0700

    edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { red } from '@material-ui/core/colors';
 import PropTypes from 'prop-types';
 
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+
 function Header(props) {
     const headerStyles = {
         backgroundColor: 'blue',
